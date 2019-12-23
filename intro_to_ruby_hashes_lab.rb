3c7de0de0c84298516f0a8558635e14cfb031fff
Natalia  Wit <natwit95@gmail.com>
2019-12-23 05:07:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,22 +1,27 @@
 def new_hash
-  # return an empty hash
+{}
 end
+new_hash
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {:hash => "hi"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  {:id => 1}
+
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+ {key => value}
 end
+
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
